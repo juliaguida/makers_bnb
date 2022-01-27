2262406a0e1341055fe9a312f50f67392eeda62e
ktn0817 <kashif.atcha@hotmail.co.uk>
2022-01-27 16:27:58 +0000

    added price and availability to list

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 gem 'capybara'
+gem 'faker'
 gem 'launchy'
 gem 'pg'
 gem 'rspec'
--- spec/features/viewing_all_spaces_spec.rb
+++ spec/features/viewing_all_spaces_spec.rb
@@ -2,9 +2,9 @@ feature 'viewing all spaces' do
       
   scenario 'So we can view all descriptions of spaces' do
     connection = PG.connect(dbname: 'makers_bnb_test')
-    connection.exec("INSERT INTO spaces (description, name) VALUES ('Three bed house', 'Summer house');")
-    connection.exec("INSERT INTO spaces (description, name) VALUES ('Two bed luxury house', 'Koala house');")
-    connection.exec("INSERT INTO spaces (description, name) VALUES ('One bed cosy house', 'Penelope house');")
+    connection.exec("INSERT INTO spaces (description, name, price, date1, date2) VALUES ('Three bed house', 'Summer house', '100', '2022-01-26', '2022-01-27');")
+    connection.exec("INSERT INTO spaces (description, name, price, date1, date2) VALUES ('Two bed luxury house', 'Koala house', '200', '2022-01-26', '2022-01-27');")
+    connection.exec("INSERT INTO spaces (description, name, price, date1, date2) VALUES ('One bed cosy house', 'Penelope house', '150', '2022-01-26', '2022-01-27');")
     visit ('/spaces')
 
     expect(page).to have_content 'Three bed house'
@@ -13,5 +13,16 @@ feature 'viewing all spaces' do
     expect(page).to have_content 'Summer house'
     expect(page).to have_content 'Koala house'
     expect(page).to have_content 'Penelope house'
+    expect(page).to have_content '100'
+    expect(page).to have_content '200'
+    expect(page).to have_content '150'
+    expect(page).to have_content '2022-01-26'
+    expect(page).to have_content '2022-01-27'
+    expect(page).to have_content '2022-01-26'
+    expect(page).to have_content '2022-01-27'
+    expect(page).to have_content '2022-01-26'
+    expect(page).to have_content '2022-01-27'
+    click_button('List a Space') 
+    expect(page).to have_current_path('/spaces/new')
   end
 end

80db3caefbfcc7dd781f09c21be19eb842bac70c
Edward Andress <eddie_andress@hotmail.com>
2022-01-28 10:41:52 +0000

    created user model and added SQL for users table

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+  def self.create
+    # add an entry to the database
+  end
+
+  def self.find_by_email
+    # find a record based on the email address provided
+    # return a hash that represents the user
+  end
+end
---
+++ spec/user_spec.rb
@@ -0,0 +1,11 @@
+describe User do
+  describe '.create' do
+    it 'adds an entry to the users table' do
+    end
+  end
+
+    describe '.find_by_email' do
+      it 'finds a user record based on the email address provided' do
+      end
+    end
+end
